1450b6b94b284b23f24d24fbfe942aaa62e7a6e4
Kaisa Huusko <kaisa.huusko@stuntfreaksteam.org>
2019-07-31 20:29:37 +0300

    Usage and styling of ToolTipCard

--- src/pages/build/Sets/GearCard.tsx
+++ src/pages/build/Sets/GearCard.tsx
@@ -1,62 +1,18 @@
 import React, { useContext } from "react";
 import { Card, Divider, Popover } from "antd";
 import styled from "styled-components";
-
 import { BuildContext } from "../BuildStateContext";
 import { IGearSlot, IGearSlotProps } from "../../../components/GearSlot";
+import ToolTipCard from "../../../components/ToolTipCard";
 
-interface IGearCard extends IGearSlotProps {
-    
-}
+interface IGearCard extends IGearSlotProps {}
 
-{/**** MOVE THESE STYLES TO GLOBAL STYLES ****/}
-const ToolTip = styled.div`
-  display: "flex";
-  width: 400px;
-  position: relative;
-`
-
-const MyAvatar = styled.img`
-  width: 40px;
-  height: 40px;
-  border-radius: 3px;
-`;
-
-const AvatarContainer = styled.div`
-  padding-right: 16px;
-`;
-
-const Description = styled.div`
-  font-size: 14px;
-  line-height: 1.5;
-  text-align: left;
-`;
-
-const Title = styled.div`
-  font-size: 16px;
-  line-height: 1.5;
-  font-weight: 500;
-  color: rgba(0, 0, 0, 0.85);
-  margin-bottom: 8px;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-  text-align: left;
-`;
-
-export default (gearProp: IGearCard) => {
+export default ({ slot }: IGearCard) => {
     return (
-        <ToolTip>
-            <AvatarContainer>
-            <MyAvatar
-                title={gearProp.slot.set ? gearProp.slot.set.type : "Title"}
-                src={gearProp.slot.icon}
-            />
-            </AvatarContainer>
-            <Description>
-            <div style={{ display: "flex", flexDirection: "column" }}>
-                {gearProp.slot.set ? gearProp.slot.set.slug : "Set"}
-            </div>
-            </Description>
-        </ToolTip>
+        <ToolTipCard
+          title={slot.set ? slot.set.name : "Undefined"}
+          image={slot.icon ? slot.icon : "Undefined"}
+          description={slot.set ? slot.set.slug : "Undefined"}
+        />
     )}
 
