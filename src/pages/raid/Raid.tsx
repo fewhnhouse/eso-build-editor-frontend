19eb8826b235418f98c8db828743bb6175545688
Kaisa Huusko <kaisa.huusko@stuntfreaksteam.org>
2019-08-04 02:08:58 +0300

    Raid page master update

--- src/pages/raid/Raid.tsx
+++ src/pages/raid/Raid.tsx
@@ -1,14 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import { Layout, Typography, Divider, Input, InputNumber, Radio, Form } from "antd";
-import Flex from "../../components/Flex";
-import TextArea from "antd/lib/input/TextArea";
-
-/*
-A screen where you set name, description, group size, content (pvp/pve/bg) and access rights
-*/
-const { Content } = Layout;
-const { Title, Text } = Typography;
+import { Layout, Icon, Button, Steps, Tooltip } from "antd";
+import { RouteComponentProps, Redirect } from "react-router";
+const { Footer, Content } = Layout;
+const { Step } = Steps;
 
 const Container = styled(Content)`
   display: flex;
@@ -18,64 +13,88 @@ const Container = styled(Content)`
   width: 100%;
   overflow: auto;
   height: calc(100vh - 178px);
-  color: ${props => props.theme.mainBg};
+  color: rgb(155, 155, 155);
 `;
 
-const Wrapper = styled(Flex)`
- width: 100%;
-`
-const LeftSide = styled(Flex)`
-  width: 500px;
-  max-width: 800px;
-  text-align: left;
-`
-const RightSide = styled(Flex)`
-  width: 500px;
-  max-width: 800px;
-  text-align: left;
-`
-const ContentFlex = styled(Flex)``
+const TabButton = styled(Button)`
+  margin: 0px 10px;
+`;
+export default ({ match }: RouteComponentProps<{ id: string }>) => {
+  const { id } = match.params;
 
-export default () => {
+  const [tab, setTab] = useState(0);
+
+  const handlePrevClick = () => {
+    setTab(tabIndex => tabIndex - 1);
+  };
+
+  const handleNextClick = () => {
+    setTab(tabIndex => tabIndex + 1);
+  };
+
+  const setTooltipTitle = () => {
+    switch (tab) {
+      case 0:
+        return "Select some general Information.";
+      case 1:
+        return "Select the builds of your Setup.";
+      case 2:
+        return "Confirm and Save.";
+    }
+  };
+
   return (
-    <Container>
-      <Title level={1}>Raid editor</Title>
-      <Text>Set up the basics for your raid group</Text>
-      <Wrapper direction={"row"} justify={"space-around"} align={"flex-start"}>
-        <LeftSide direction={"column"} justify={"left"} align={"left"} >
-          <Title level={4}>Group name</Title>
-          <Input placeholder="Puffymuffins"></Input>
-          <Divider/>
-          <Title level={4}>Description</Title>
-          <TextArea placeholder="12 little elephants marching on"></TextArea>
-          <Divider/>
-          <ContentFlex direction={"row"} justify={"space-around"} align={"flex-start"}>
-            <div style={{flex: 1}}>
-              <Title level={4}>Group size (1-24)</Title>
-              <InputNumber min={1} max={24} defaultValue={4}></InputNumber>
-            </div>
-            <div style={{flex: 1}}>
-              <Title level={4}>Content</Title>
-              <Radio.Group buttonStyle={"solid"} defaultValue={"pve"}>
-                <Radio.Button value={"pve"}>PvE</Radio.Button>
-                <Radio.Button value={"pvp"}>PvP</Radio.Button>
-              </Radio.Group>
-            </div>
-          </ContentFlex>
-          <Divider/>
-        </LeftSide>
-        <RightSide direction={"column"} justify={""} align={"left"}>
-          <Title level={4}>Access rights</Title>
-          <Form labelCol={{ span: 5 }} wrapperCol={{ span: 15 }}>
-            <Form.Item label="Username">
-              <Input placeholder=""></Input>
-            </Form.Item>
-            <Form.Item label="Password">
-              <Input placeholder=""></Input>
-            </Form.Item>
-          </Form>
-        </RightSide>
-      </Wrapper>
-    </Container>
-  )
+    <>
+      <Container>
+        <div>Raid</div>
+      </Container>
+      <Footer
+        style={{
+          display: "flex",
+          zIndex: 100,
+          alignItems: "center",
+          boxShadow: "0 -2px 6px 0 rgba(0, 0, 0, 0.1)"
+        }}
+      >
+        <TabButton
+          onClick={handlePrevClick}
+          disabled={tab === 0}
+          size="large"
+          type="primary"
+        >
+          <Icon type="left" />
+          Prev
+        </TabButton>
+        <Steps progressDot current={tab}>
+          <Step
+            style={{ whiteSpace: "nowrap" }}
+            title="General Information"
+            description="Add general Raid info."
+          />
+          <Step
+            style={{ whiteSpace: "nowrap" }}
+            title="Builds"
+            description="Add builds to your setup."
+          />
+          <Step
+            title="Review"
+            style={{ whiteSpace: "nowrap" }}
+            description="Review and save."
+          />
+        </Steps>
+        <Tooltip title={setTooltipTitle()}>
+          <TabButton
+            onClick={handleNextClick}
+            disabled={false}
+            size="large"
+            type="primary"
+          >
+            <Icon type="right" />
+            Next
+          </TabButton>
+        </Tooltip>
+        <Redirect to={`/raid/${tab}`} push />
+      </Footer>
+    </>
+  );
 };
