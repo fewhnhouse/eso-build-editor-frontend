50da3b27b6939983d77fe2f6dbcfb890ccbc36b1
Felix Wohnhaas <felix.wohnhaas@outlook.com>
2019-08-06 11:54:58 +0200

    login adjustment.

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,8 +1,8 @@
-import { Form, Icon, Input, Button, Checkbox, message } from 'antd'
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import gql from 'graphql-tag'
-import { useMutation } from '@apollo/react-hooks'
+import { Form, Icon, Input, Button, Checkbox, message } from 'antd';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/react-hooks';
 
 const LOGIN = gql`
   mutation login($email: String!, $password: String!) {
@@ -13,50 +13,50 @@ const LOGIN = gql`
       }
     }
   }
-`
+`;
 const StyledForm = styled(Form)`
   display: flex;
   width: '250px';
   flex-direction: column;
-`
+`;
 
 const LoginForm = ({ form }: { form: any }) => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const handleSubmit = (e: React.SyntheticEvent<any>) => {
-    e.preventDefault()
-    login()
+    e.preventDefault();
+    login({ variables: { email, password } });
     form.validateFields((err: any, values: any) => {
       if (!err) {
-        console.log('Received values of form: ', values)
+        console.log('Received values of form: ', values);
       }
-    })
-  }
+    });
+  };
 
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value)
-  }
+    setEmail(e.target.value);
+  };
 
   const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value)
-  }
+    setPassword(e.target.value);
+  };
 
   const [login, { error, data }] = useMutation<
     { login: { token: string } },
     { email: string; password: string }
-  >(LOGIN, { variables: { email, password } })
+  >(LOGIN, { variables: { email, password } });
 
   if (error) {
-    message.error(error.message)
+    message.error(error.message);
   }
   if (data) {
-    console.log('data', data)
-    localStorage.setItem('token', data.login.token)
+    console.log('data', data);
+    localStorage.setItem('token', data.login.token);
   }
 
-  const { getFieldDecorator } = form
+  const { getFieldDecorator } = form;
   return (
-    <StyledForm onSubmit={handleSubmit} className='login-form'>
+    <StyledForm onSubmit={handleSubmit} className="login-form">
       <Form.Item>
         {getFieldDecorator('username', {
           rules: [{ required: true, message: 'Please input your username!' }],
@@ -64,8 +64,8 @@ const LoginForm = ({ form }: { form: any }) => {
           <Input
             value={email}
             onChange={handleEmailChange}
-            prefix={<Icon type='email' style={{ color: 'rgba(0,0,0,.25)' }} />}
-            placeholder='Email'
+            prefix={<Icon type="email" style={{ color: 'rgba(0,0,0,.25)' }} />}
+            placeholder="Email"
           />
         )}
       </Form.Item>
@@ -76,9 +76,9 @@ const LoginForm = ({ form }: { form: any }) => {
           <Input
             value={password}
             onChange={handlePasswordChange}
-            prefix={<Icon type='lock' style={{ color: 'rgba(0,0,0,.25)' }} />}
-            type='password'
-            placeholder='Password'
+            prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+            type="password"
+            placeholder="Password"
           />
         )}
       </Form.Item>
@@ -87,24 +87,24 @@ const LoginForm = ({ form }: { form: any }) => {
           valuePropName: 'checked',
           initialValue: true,
         })(<Checkbox>Remember me</Checkbox>)}
-        <a className='login-form-forgot' href='/resetPassword'>
+        <a className="login-form-forgot" href="/resetPassword">
           Forgot password
         </a>
       </Form.Item>
       <Form.Item>
         <Button
           style={{ width: '100%' }}
-          type='primary'
-          htmlType='submit'
-          className='login-form-button'
+          type="primary"
+          htmlType="submit"
+          className="login-form-button"
         >
           Log in
         </Button>
       </Form.Item>
     </StyledForm>
-  )
-}
+  );
+};
 
-const WrappedNormalLoginForm = Form.create({ name: 'normal_login' })(LoginForm)
+const WrappedNormalLoginForm = Form.create({ name: 'normal_login' })(LoginForm);
 
-export default WrappedNormalLoginForm
+export default WrappedNormalLoginForm;
