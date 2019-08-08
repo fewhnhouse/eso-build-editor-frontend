5e395b57319f4f90a02535771e2d6510c6f3537d
Felix Wohnhaas <felix.wohnhaas@outlook.com>
2019-08-08 21:21:41 +0200

    some more minor improvements.

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import { Layout, Typography, Divider, Input } from 'antd'
-import NotLoggedInHome from './NotLoggedInHome';
-import LoggedInHome from './LoggedInHome';
+import NotLoggedInHome from './NotLoggedInHome'
+import LoggedInHome from './LoggedInHome'
 
 const { Content } = Layout
-const { Text } = Typography
-const { Search } = Input;
 
 const Container = styled(Content)`
   display: flex;
@@ -19,48 +17,13 @@ const Container = styled(Content)`
   color: ${props => props.theme.mainBg};
 `
 
-const StyledTitle = styled.h1`
-  margin-top: 20px;
-  font-size: 50px;
-`
-
-const HomeHeader = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 200px;
-  min-height: 100px;
-  margin: 0;
-  padding: 0;
-  background-color: sandybrown;
-`
-
 interface IHomeProps {
   loggedIn: boolean
 }
-const Home = ({loggedIn}: IHomeProps) => {
+const Home = ({ loggedIn }: IHomeProps) => {
   return (
-    <Container>
-        { loggedIn ? 
-          ""
-        :
-        <>
-          <HomeHeader>
-            <Search size="large" style={{maxWidth: "400px", maxHeight: "20px"}} placeholder="Search for builds and raids..." />
-          </HomeHeader>
-          <StyledTitle>Welcome to FIST Build Editor</StyledTitle>
-            <Text style={{maxWidth: "400px"}}>
-              Build Editor allows you to create and save builds and combine them to complete raid setups -
-              to be shared with friends, guilds and communities, or saved privately as you choose.
-            </Text>
-            <Divider />
-          </>
-        }
-    { loggedIn ? <LoggedInHome /> : <NotLoggedInHome /> }
-    </Container>
+    <Container>{loggedIn ? <LoggedInHome /> : <NotLoggedInHome />}</Container>
   )
 }
 
-export default Home;
+export default Home
--- src/pages/home/NotLoggedInHome.tsx
+++ src/pages/home/NotLoggedInHome.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Typography, Divider, Icon, Button } from 'antd'
+import { Typography, Divider, Icon, Button, Input } from 'antd'
 import Flex from '../../components/Flex'
 
-const { Title, Text } = Typography;
+const { Search } = Input
+
+const { Title, Text } = Typography
 
 const Wrapper = styled(Flex)`
-    padding-top: 20px;
-    width: 100%;
-    flex-wrap: wrap;
+  padding-top: 20px;
+  width: 100%;
+  flex-wrap: wrap;
 `
 
 const LeftSide = styled(Flex)`
@@ -34,6 +36,10 @@ const Center = styled(Flex)`
   height: 300px;
   background-color: #d1d1d1;
 `
+const StyledTitle = styled.h1`
+  margin-top: 20px;
+  font-size: 50px;
+`
 
 const RightSide = styled(Flex)`
   flex: 1;
@@ -47,26 +53,57 @@ const RightSide = styled(Flex)`
   height: 300px;
 `
 
+const HomeHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 200px;
+  min-height: 100px;
+  margin: 0;
+  padding: 0;
+  background-color: sandybrown;
+`
+
 export default () => {
-    return (
-        <Wrapper direction={"row"} justify={""} align={"flex-start"}>
-            <LeftSide direction={"column"} justify={"space-between"} align={""}>
-                <Title level={3}>Builds</Title>
-                <Text>
-                    Create, share, save and edit builds
-                </Text>
-                <Button type="primary" ghost={true }>Create</Button>
-            </LeftSide>
-            <Center direction={"column"} justify={"space-between"} align={""}>
-                <Title level={3}>Overview</Title>
-            </Center>
-            <RightSide direction={"column"} justify={"space-between"} align={""}>
-                <Title level={3}>Raids</Title>
-                <Text>
-                    Create, share, save and edit raids
-                </Text>
-                <Button type="primary" ghost={true }>Create</Button>
-            </RightSide>
-        </Wrapper>
-    )
+  return (
+    <>
+      <HomeHeader>
+        <Search
+          size='large'
+          style={{ maxWidth: '400px', maxHeight: '20px' }}
+          placeholder='Search for builds and raids...'
+        />
+      </HomeHeader>
+
+      <StyledTitle>Welcome to FIST Build Editor</StyledTitle>
+      <Text style={{ maxWidth: '400px' }}>
+        Build Editor allows you to create and save builds and combine them to
+        complete raid setups - to be shared with friends, guilds and
+        communities, or saved privately as you choose.
+      </Text>
+      <Divider />
+
+      <Wrapper direction={'row'} justify={''} align={'flex-start'}>
+        <LeftSide direction={'column'} justify={'space-between'} align={''}>
+          <Title level={3}>Builds</Title>
+          <Text>Create, share, save and edit builds</Text>
+          <Button type='primary' ghost={true}>
+            Create
+          </Button>
+        </LeftSide>
+        <Center direction={'column'} justify={'space-between'} align={''}>
+          <Title level={3}>Overview</Title>
+        </Center>
+        <RightSide direction={'column'} justify={'space-between'} align={''}>
+          <Title level={3}>Raids</Title>
+          <Text>Create, share, save and edit raids</Text>
+          <Button type='primary' ghost={true}>
+            Create
+          </Button>
+        </RightSide>
+      </Wrapper>
+    </>
+  )
 }
