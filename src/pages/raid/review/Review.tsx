eee5cb1be37b40838b5561dd81f39984674876b6
Kaisa Huusko <kaisa.huusko@stuntfreaksteam.org>
2019-08-09 16:30:38 +0300

    Raid review page layout

--- src/pages/raid/review/Review.tsx
+++ src/pages/raid/review/Review.tsx
@@ -1,5 +1,110 @@
 import React from 'react';
+import styled from 'styled-components';
+import Flex from '../../../components/Flex';
+import { Typography, Divider, Card } from 'antd';
+import { defaultRaidState, IRaidState } from '../../raid/RaidStateContext';
 
-export default () => {
-  return <div>asd</div>;
+const { Title, Text } = Typography;
+
+const Wrapper = styled(Flex)`
+  width: 100%;
+`
+
+const RaidHeader = styled(Flex)`
+  flex: 1;
+  width: 100%;
+  margin-top: 20px;
+  margin-bottom: 10px;
+`
+const RaidContent = styled(Flex)`
+  width: 100%;
+  flex-wrap: wrap;
+  max-height: 600px;
+`
+
+const LeftSide = styled(Flex)`
+  flex: 1;
+  margin: 20px;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 5px 2px rgba(0, 0, 0, 0.2);
+  height: 100%;
+  background-color: white;
+  min-width: 300px;
+`
+
+const RightSide = styled(Flex)`
+  flex: 2;
+  flex-wrap: wrap;
+  margin: 20px;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 5px 2px rgba(0, 0, 0, 0.2);
+  height: 100%;
+  background-color: white;
+  overflow-y: scroll;
+  min-width: 300px;
+`
+const CardList = styled(Flex)`
+  flex-wrap: wrap;
+`
+
+const StyledCard = styled(Card)`
+  width: 200px;
+  margin: 10px;
+`
+
+const Review = () => {
+
+  const raidState = localStorage.getItem('raidState');
+  const parsedRaidState: IRaidState = raidState
+    ? JSON.parse(raidState)
+    : defaultRaidState;
+    console.log(parsedRaidState);
+  
+  const {
+    name,
+    applicationArea,
+    groupSize,
+    description,
+    roles
+  } = parsedRaidState;
+
+  return (
+    <Wrapper direction={"column"} justify={""} align={""}>
+      <RaidHeader direction={"row"} justify={""} align={""}>
+        <Title level={1}>Raid review</Title>
+      </RaidHeader>
+      <RaidContent direction={"row"} justify={""} align={"flex-start"}>
+        <LeftSide direction={""} justify={""} align={"left"}>
+          <Title level={2} style={{textAlign: "center"}}>{name}</Title>
+          <Title level={3}>Group size:</Title>
+          <Text>{groupSize}</Text>
+          <Title level={3}>Application area:</Title>
+          <Text>{applicationArea}</Text>
+          <Title level={3}>Description:</Title>
+          <Text>{description}</Text>
+        </LeftSide>
+        <RightSide direction={"row"} justify={"center"} align={""}>
+          <Title level={2}>Builds</Title>
+          {roles.map(role => {
+            return (
+              <>
+                <Divider>{role.roleName}</Divider>
+                <CardList direction={"row"} justify={""} align={""}>
+                  {role.builds.map(build => {
+                    return (
+                      <StyledCard hoverable title={build.name}>{build.description}</StyledCard>
+                    )
+                  })}
+                </CardList>
+              </>
+            )
+          })}
+        </RightSide>
+      </RaidContent>
+    </Wrapper>
+  )
 };
+
+export default Review;
