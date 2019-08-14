14b6a65e2e4a17db9f33c25c6be3bb7517298a3e
Felix Wohnhaas <felix.wohnhaas@outlook.com>
2019-08-14 13:16:26 +0200

    minor changes.

--- src/pages/buildreview/BuildReviewDetails.tsx
+++ src/pages/buildreview/BuildReviewDetails.tsx
@@ -1,15 +1,14 @@
-import React from "react";
-import styled from "styled-components";
-import Flex from "../../components/Flex";
-import { Typography, Layout, Divider } from "antd";
-import { chooseClass } from "../../util/utils";
-import GearView from "../../components/GearView";
-import SkillView from "../../components/SkillView";
-import { ABILITY_BAR_ONE, ABILITY_BAR_TWO } from "../build/Skills/AbilityBar";
-import { IBuildState } from "../build/BuildStateContext";
+import React from 'react'
+import styled from 'styled-components'
+import Flex from '../../components/Flex'
+import { Typography, Layout, Divider } from 'antd'
+import GearView from '../../components/GearView'
+import SkillView from '../../components/SkillView'
+import { ABILITY_BAR_ONE, ABILITY_BAR_TWO } from '../build/Skills/AbilityBar'
+import { IBuildState } from '../build/BuildStateContext'
 
-const { Content } = Layout;
-const { Title, Text } = Typography;
+const { Content } = Layout
+const { Title, Text } = Typography
 
 const Container = styled(Content)`
   display: flex;
@@ -20,152 +19,155 @@ const Container = styled(Content)`
   overflow: auto;
   height: calc(100vh - 178px);
   color: ${props => props.theme.mainBg};
-`;
+`
 const StyledTitle = styled(Title)`
   margin-bottom: 5px !important;
-`;
-const Wrapper = styled(Flex)``;
+`
+const Wrapper = styled(Flex)``
 const RightSide = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   max-width: 500px;
-`;
+`
 const LeftSide = styled.div`
   max-width: 500px;
-`;
+`
 const SkillsView = styled.div`
   margin-bottom: 10px;
-`;
+`
 const MiscView = styled.div`
   max-width: 500px;
-`;
+`
 const ClassImg = styled.img`
   width: 30px;
   height: 30px;
   margin-right: 5px;
-`;
+`
 
 interface IDetailViewProps {
-    loadedData: IBuildState
+  loadedData: IBuildState
 }
 
-const BuildReviewDetails = ({loadedData}: IDetailViewProps) => {
+const BuildReviewDetails = ({ loadedData }: IDetailViewProps) => {
+  const {
+    name,
+    bigPieceSelection,
+    smallPieceSelection,
+    frontbarSelection,
+    backbarSelection,
+    jewelrySelection,
+    mundus,
+    buff,
+    esoClass,
+    race,
+    mainResource,
+    applicationArea,
+    role,
+    description,
+    newBarOne,
+    newBarTwo,
+  } = loadedData
 
-    const {
-        name,
-        bigPieceSelection,
-        smallPieceSelection,
-        frontbarSelection,
-        backbarSelection,
-        jewelrySelection,
-        mundus,
-        buff,
-        esoClass,
-        race,
-        mainResource,
-        applicationArea,
-        role,
-        description,
-        newBarOne,
-        newBarTwo
-      } = loadedData;
+  const selectedSetup = [
+    {
+      id: 'bigpieces',
+      label: 'Big Pieces',
+      data: bigPieceSelection || [],
+    },
+    {
+      id: 'smallpieces',
+      label: 'Small Pieces',
+      data: smallPieceSelection || [],
+    },
+    { id: 'jewelry', label: 'Jewelry', data: jewelrySelection || [] },
+    {
+      id: 'frontbar',
+      label: 'Frontbar',
+      data: frontbarSelection || [],
+    },
+    { id: 'backbar', label: 'Backbar', data: backbarSelection || [] },
+  ]
 
-    const selectedSetup = [
-      {
-        id: 'bigpieces',
-        label: 'Big Pieces',
-        data: bigPieceSelection || [],
-      },
-      {
-        id: 'smallpieces',
-        label: 'Small Pieces',
-        data: smallPieceSelection || [],
-      },
-      { id: 'jewelry', label: 'Jewelry', data: jewelrySelection || [] },
-      {
-        id: 'frontbar',
-        label: 'Frontbar',
-        data: frontbarSelection || [],
-      },
-      { id: 'backbar', label: 'Backbar', data: backbarSelection || [] },
-    ];
-  
-    return (
-      <Container>
-        <Title level={1}>
-          <ClassImg
-            title={esoClass ? esoClass : "Class undefined"}
-            src={chooseClass(esoClass ? esoClass : "")}
-          />
-          {name ? name : "Name undefined"}<br />
-          {esoClass ? esoClass : "Class undefined"}
-        </Title>
-        <Title level={4}>
-          {race ? race : ""} {applicationArea? applicationArea : ""} <br />
-          {mainResource ? mainResource : ""} {role ? role : ""} <br/>
-          {description ? description : ""}
-        </Title>
-        <Wrapper
-          direction="row"
-          align="flex-start"
-          justify="space-evenly"
-          wrap
-          fluid
-        >
-          <LeftSide>
-            <Divider />
-            <GearView disabled setups={selectedSetup} />
-          </LeftSide>
-          <RightSide>
-            <Divider />
-            <SkillsView>
-              <StyledTitle level={4}>Skills</StyledTitle>
-              <SkillView
-                id={ABILITY_BAR_ONE}
-                disabled
-                skillSlots={newBarOne}
-              />
-              <SkillView
-                id={ABILITY_BAR_TWO}
-                disabled
-                skillSlots={newBarTwo}
-              />
-            </SkillsView>
-            <Divider />
-            <MiscView>
-              <StyledTitle level={4}>Mundus</StyledTitle>
-                {mundus ? 
-                <>
-                    <Flex direction="row" justify="center" align="center">
-                    <ClassImg src={`${process.env.REACT_APP_IMAGE_SERVICE}/mundusStones${mundus.icon}`} />
-                    <Text strong>{mundus.name}</Text>
-                    <br />
-                    </Flex>
-                    <Text>
-                    {mundus.effect} by {mundus.value}.
-                    </Text>
-                    <Divider />
-                    <StyledTitle level={4}>Consumables</StyledTitle>
-                </>
-                : "Mundus undefined."} <br />
-                {buff ? 
-                    <>
-                        <ClassImg src={`${process.env.REACT_APP_IMAGE_SERVICE}/${buff.type}s/${buff.icon}`} />
-                        <Text strong>
-                            {buff.name} ({buff.type})
-                        </Text>
-                        <br />
-                        <Text>
-                            {buff.buffDescription}
-                        </Text>
-                    </>
-                : "Buff undefined." }
-            </MiscView>
-          </RightSide>
-        </Wrapper>
-      </Container>
-    )
+  return (
+    <Container>
+      <Title level={1}>
+        <ClassImg
+          title={esoClass ? esoClass : 'Class undefined'}
+          src={`${process.env.REACT_APP_IMAGE_SERVICE}/classes/${esoClass}.png`}
+        />
+        {name ? name : 'Name undefined'}
+        <br />
+        {esoClass ? esoClass : 'Class undefined'}
+      </Title>
+      <Title level={4}>
+        {race ? race : ''} {applicationArea ? applicationArea : ''} <br />
+        {mainResource ? mainResource : ''} {role ? role : ''} <br />
+        {description ? description : ''}
+      </Title>
+      <Wrapper
+        direction='row'
+        align='flex-start'
+        justify='space-evenly'
+        wrap
+        fluid
+      >
+        <LeftSide>
+          <Divider />
+          <GearView disabled setups={selectedSetup} />
+        </LeftSide>
+        <RightSide>
+          <Divider />
+          <SkillsView>
+            <StyledTitle level={4}>Skills</StyledTitle>
+            <SkillView id={ABILITY_BAR_ONE} disabled skillSlots={newBarOne} />
+            <SkillView id={ABILITY_BAR_TWO} disabled skillSlots={newBarTwo} />
+          </SkillsView>
+          <Divider />
+          <MiscView>
+            <StyledTitle level={4}>Mundus</StyledTitle>
+            {mundus ? (
+              <>
+                <Flex direction='row' justify='center' align='center'>
+                  <ClassImg
+                    src={`${process.env.REACT_APP_IMAGE_SERVICE}/mundusStones${
+                      mundus.icon
+                    }`}
+                  />
+                  <Text strong>{mundus.name}</Text>
+                  <br />
+                </Flex>
+                <Text>
+                  {mundus.effect} by {mundus.value}.
+                </Text>
+                <Divider />
+                <StyledTitle level={4}>Consumables</StyledTitle>
+              </>
+            ) : (
+              'Mundus undefined.'
+            )}{' '}
+            <br />
+            {buff ? (
+              <>
+                <ClassImg
+                  src={`${process.env.REACT_APP_IMAGE_SERVICE}/${buff.type}s/${
+                    buff.icon
+                  }`}
+                />
+                <Text strong>
+                  {buff.name} ({buff.type})
+                </Text>
+                <br />
+                <Text>{buff.buffDescription}</Text>
+              </>
+            ) : (
+              'Buff undefined.'
+            )}
+          </MiscView>
+        </RightSide>
+      </Wrapper>
+    </Container>
+  )
 }
 
-export default BuildReviewDetails;
+export default BuildReviewDetails
