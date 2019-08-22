b5a6a5a4ee759206c9bcc63e56538bc689a739a8
Felix Wohnhaas <felix.wohnhaas@outlook.com>
2019-08-23 00:00:18 +0200

    add better slotting.

--- src/pages/build/reducers/setBarReducer.ts
+++ src/pages/build/reducers/setBarReducer.ts
@@ -6,6 +6,7 @@ import {
   WeaponType,
   OnehandedWeapon,
   TwohandedWeapon,
+  SetTab,
 } from '../BuildStateContext';
 import { ISet } from '../../../components/GearSlot';
 
@@ -17,116 +18,89 @@ export const setBarReducer = (state: IBuildState, action: IBuildAction) => {
         slot,
         group,
         icon,
-        // type,
-        // id,
         weaponType,
       }: {
-        id: string;
         set: ISet;
         slot: Slot;
         group: string;
         icon: string;
-        type: SlotType;
         weaponType: OnehandedWeapon | TwohandedWeapon | undefined;
       } = action.payload;
-      if (group === 'jewelry') {
+      const { setTabKey, frontbarSelection, backbarSelection } = state!;
+      if (setTabKey === SetTab.jewelry) {
         return {
           ...state,
           jewelrySelection: state.jewelrySelection.map(jewelry =>
             jewelry.slot === slot
               ? {
-                  selectedSet: set,
-                  slot,
-                  icon,
-                  glyph: jewelry.glyph,
-                  trait: jewelry.trait,
-                  id: jewelry.id,
-                }
+                selectedSet: set,
+                slot,
+                icon,
+                glyph: jewelry.glyph,
+                trait: jewelry.trait,
+                id: jewelry.id,
+              }
               : jewelry
           ),
         };
-      } else if (group === 'frontbar') {
+      } else if (setTabKey === SetTab.frontbar) {
+        const actualSlot = slot === Slot.eitherHand ? frontbarSelection[0].selectedSet ? frontbarSelection[1].selectedSet ? Slot.mainHand : Slot.offHand : Slot.mainHand : slot
+
         if (state.weaponType === WeaponType.twohanded) {
           return {
             ...state,
             frontbarSelection: state.frontbarSelection.map(frontbar =>
-              frontbar.slot === slot
+              frontbar.slot === actualSlot
                 ? {
-                    selectedSet: set,
-                    slot,
-                    icon,
-                    type: state.weaponType,
-                    glyph: frontbar.glyph,
-                    trait: frontbar.trait,
-                    id: frontbar.id,
-                    weaponType,
-                  }
+                  selectedSet: set,
+                  slot: actualSlot,
+                  icon,
+                  type: state.weaponType,
+                  glyph: frontbar.glyph,
+                  trait: frontbar.trait,
+                  id: frontbar.id,
+                  weaponType,
+                }
                 : {
-                    selectedSet: undefined,
-                    slot: frontbar.slot,
-                    type: undefined,
-                    icon: undefined,
-                    trait: undefined,
-                    glyph: undefined,
-                    weaponType: undefined,
-                    id: undefined,
-                  }
+                  selectedSet: undefined,
+                  slot: frontbar.slot,
+                  type: undefined,
+                  icon: undefined,
+                  trait: undefined,
+                  glyph: undefined,
+                  weaponType: undefined,
+                  id: undefined,
+                }
             ),
           };
         }
         return {
           ...state,
           frontbarSelection: state.frontbarSelection.map(frontbar =>
-            frontbar.slot === slot
+            frontbar.slot === actualSlot
               ? {
-                  selectedSet: set,
-                  slot,
-                  icon,
-                  type: state.weaponType,
-                  glyph: frontbar.glyph,
-                  trait: frontbar.trait,
-                  weaponType,
-                  id: frontbar.id,
-                }
+                selectedSet: set,
+                slot: actualSlot,
+                icon,
+                type: state.weaponType,
+                glyph: frontbar.glyph,
+                trait: frontbar.trait,
+                weaponType,
+                id: frontbar.id,
+              }
               : frontbar
           ),
         };
-      } else if (group === 'backbar') {
+      } else if (setTabKey === SetTab.backbar) {
+        const actualSlot = slot === Slot.eitherHand ? backbarSelection[0].selectedSet ? backbarSelection[1].selectedSet ? Slot.mainHand : Slot.offHand : Slot.mainHand : slot
         if (state.weaponType === WeaponType.twohanded) {
           return {
             ...state,
             backbarSelection: state.backbarSelection.map(backbar =>
-              backbar.slot === slot
+              backbar.slot === actualSlot
                 ? {
-                    selectedSet: set,
-                    slot,
-                    icon,
-                    type: state.weaponType,
-                    glyph: backbar.glyph,
-                    trait: backbar.trait,
-                    weaponType,
-                    id: backbar.id,
-                  }
-                : {
-                    selectedSet: undefined,
-                    slot: backbar.slot,
-                    type: undefined,
-                    icon: undefined,
-                    trait: undefined,
-                    glyph: undefined,
-                    weaponType: undefined,
-                    id: undefined,
-                  }
-            ),
-          };
-        }
-        return {
-          ...state,
-          backbarSelection: state.backbarSelection.map(backbar =>
-            backbar.slot === slot
-              ? {
                   selectedSet: set,
-                  slot,
+                  slot: actualSlot,
                   icon,
                   type: state.weaponType,
                   glyph: backbar.glyph,
@@ -134,40 +108,67 @@ export const setBarReducer = (state: IBuildState, action: IBuildAction) => {
                   weaponType,
                   id: backbar.id,
                 }
+                : {
+                  selectedSet: undefined,
+                  slot: backbar.slot,
+                  type: undefined,
+                  icon: undefined,
+                  trait: undefined,
+                  glyph: undefined,
+                  weaponType: undefined,
+                  id: undefined,
+                }
+            ),
+          };
+        }
+        return {
+          ...state,
+          backbarSelection: state.backbarSelection.map(backbar =>
+            backbar.slot === actualSlot
+              ? {
+                selectedSet: set,
+                slot: actualSlot,
+                icon,
+                type: state.weaponType,
+                glyph: backbar.glyph,
+                trait: backbar.trait,
+                weaponType,
+                id: backbar.id,
+              }
               : backbar
           ),
         };
-      } else if (group === 'bigpieces') {
+      } else if (setTabKey === SetTab.armor && group === "bigpieces") {
         return {
           ...state,
           bigPieceSelection: state.bigPieceSelection.map(bigPiece =>
             bigPiece.slot === slot
               ? {
-                  selectedSet: set,
-                  slot,
-                  icon,
-                  type: state.armorType,
-                  glyph: bigPiece.glyph,
-                  trait: bigPiece.trait,
-                  id: bigPiece.id,
-                }
+                selectedSet: set,
+                slot,
+                icon,
+                type: state.armorType,
+                glyph: bigPiece.glyph,
+                trait: bigPiece.trait,
+                id: bigPiece.id,
+              }
               : bigPiece
           ),
         };
-      } else if (group === 'smallpieces') {
+      } else if (setTabKey === SetTab.armor && group === 'smallpieces') {
         return {
           ...state,
           smallPieceSelection: state.smallPieceSelection.map(smallPiece =>
             smallPiece.slot === slot
               ? {
-                  selectedSet: set,
-                  slot,
-                  icon,
-                  type: state.armorType,
-                  glyph: smallPiece.glyph,
-                  trait: smallPiece.trait,
-                  id: smallPiece.id,
-                }
+                selectedSet: set,
+                slot,
+                icon,
+                type: state.armorType,
+                glyph: smallPiece.glyph,
+                trait: smallPiece.trait,
+                id: smallPiece.id,
+              }
               : smallPiece
           ),
         };
