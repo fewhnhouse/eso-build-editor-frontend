3e5262572e5f8b1b2d7944b1cfda8c8c9c961c68
Felix Wohnhaas <felix.wohnhaas@outlook.com>
2019-08-27 22:30:44 +0200

    improve profile page further

--- src/pages/home/Profile.tsx
+++ src/pages/home/Profile.tsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
-import { Layout, Typography, Divider, Input, Button, Card } from 'antd'
+import {
+  Layout,
+  Typography,
+  Divider,
+  Input,
+  Button,
+  Card,
+  Modal,
+  notification,
+} from 'antd'
 import gql from 'graphql-tag'
 import { useQuery } from 'react-apollo'
 import Flex from '../../components/Flex'
@@ -34,28 +43,75 @@ interface IProfileProps {
 
 const Profile = ({ loggedIn }: IProfileProps) => {
   const { loading, error, data } = useQuery(ME)
+  const [action, setAction] = useState('')
+  const handleActionClick = (clickedAction: string) => () => {
+    setAction(clickedAction)
+  }
+  const handleConfirm = () => {
+    console.log(action)
+    setAction('');
+    notification.success({
+      message: action,
+      description: `${action} update successful!`,
+    })
+  }
+  const handleCancel = () => {
+    setAction('')
+  }
   return (
     <>
+      <Modal
+        title='Confirm password'
+        visible={action !== ''}
+        okText='Confirm and apply'
+        onOk={handleConfirm}
+        onCancel={handleCancel}
+      >
+        <Input.Password size='large' placeholder='Type current password...' />
+      </Modal>
       <Container>
         <Title>Hello {data && data.me ? data.me.name : ''}!</Title>
-        <Flex direction='column' justify='space-around' align='center' style={{width: "100%", margin: 20}}>
-          <Card title='Change your email' style={{ maxWidth: 500, width: "40%" }}>
+        <Flex
+          direction='column'
+          justify='space-around'
+          align='center'
+          style={{ width: '100%', margin: 20 }}
+        >
+          <Card
+            title='Change your email'
+            style={{ maxWidth: 500, width: '40%' }}
+          >
             <Text>Current email: {data && data.me ? data.me.email : ''}</Text>
             <Divider />
             <Input size='large' placeholder='Type new email...' />
-            <Button block size='large' style={{ marginTop: 20 }} type='primary'>
+            <Button
+              onClick={handleActionClick('email')}
+              block
+              size='large'
+              style={{ marginTop: 20 }}
+              type='primary'
+            >
               Update email
             </Button>
           </Card>
           <Divider />
-          <Card title='Change your password' style={{ maxWidth: 500, width: "40%" }}>
+          <Card
+            title='Change your password'
+            style={{ maxWidth: 500, width: '40%' }}
+          >
             <Input.Password size='large' placeholder='New password' />
-            <Button block size='large' style={{ marginTop: 20 }} type='primary'>
+            <Button
+              onClick={handleActionClick('password')}
+              block
+              size='large'
+              style={{ marginTop: 20 }}
+              type='primary'
+            >
               Update password
             </Button>
           </Card>
           <Divider />
-          <Card title='Delete account' style={{ maxWidth: 500, width: "40%" }}>
+          <Card title='Delete account' style={{ maxWidth: 500, width: '40%' }}>
             <Text>
               Deleting will remove the account and all associated data. Builds
               and raids created by this account will be deleted permanently.
@@ -66,7 +122,13 @@ const Profile = ({ loggedIn }: IProfileProps) => {
             <Text strong style={{ marginTop: 20 }}>
               This action cannot be undone.
             </Text>
-            <Button block size='large' style={{ marginTop: 20 }} type='danger'>
+            <Button
+              onClick={handleActionClick('account')}
+              block
+              size='large'
+              style={{ marginTop: 20 }}
+              type='danger'
+            >
               Delete account
             </Button>
           </Card>
