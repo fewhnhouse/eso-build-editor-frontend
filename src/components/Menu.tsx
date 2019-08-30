c5d993636dbb37cf631365924668e5337acbbd52
Felix Wohnhaas <felix.wohnhaas@outlook.com>
2019-08-30 11:41:27 +0200

    further improve menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import styled from 'styled-components'
-import { Layout, Menu, Icon, Dropdown, Avatar, Popover, Button } from 'antd'
+import { Layout, Menu, Icon, Avatar, Popover, Button } from 'antd'
 import { Link, RouteComponentProps, withRouter } from 'react-router-dom'
 import { LoginContext } from '../App'
 import WrappedNormalLoginForm from './LoginForm'
@@ -14,6 +14,7 @@ const StyledHeader = styled(Header)`
   background: white;
   align-items: center;
   justify-content: space-between;
+  padding-right: 10px;
   box-shadow: 0 2px 6px 0 rgba(0, 0, 0, 0.1);
 `
 
@@ -28,22 +29,9 @@ const getSelectedKey = (pathname: string, loggedIn: boolean) => {
     else if (pathname.includes('/raids')) return '3'
   }
   if (pathname.includes('/overview')) return '4'
+  if (pathname.includes('/profile')) return ''
   return '1'
 }
-const LogoutMenu = ({ handleLogout }: { handleLogout: () => void }) => (
-  <Menu>
-    <Item>
-      <Link to='/profile'>
-        <Icon type='user' style={{ marginRight: 8 }} />
-        Profile
-      </Link>
-    </Item>
-    <Item onClick={handleLogout}>
-      <Icon type='logout' />
-      Log out
-    </Item>
-  </Menu>
-)
 
 interface IMenuProps extends RouteComponentProps {
   me?: {
@@ -60,111 +48,127 @@ const NavMenu = ({ me, location }: IMenuProps) => {
   }
   return (
     <StyledHeader>
-      <div>
+      <div style={{ display: 'flex', flexDirection: 'row', width: '100%' }}>
         {loggedIn ? (
-          <Menu
-            theme='light'
-            mode='horizontal'
-            defaultSelectedKeys={[getSelectedKey(location.pathname, loggedIn)]}
-            style={{ lineHeight: '64px' }}
-          >
-            <Item key='1'>
-              <Link to='/'>Home</Link>
-            </Item>
-
-            <SubMenu
-              key='2'
-              title={
-                <span>
-                  <Icon type='setting' />
-                  Builds
-                  <Icon type='down' style={{ marginLeft: 5, fontSize: 10 }} />
-                </span>
-              }
+          <>
+            <Menu
+              theme='light'
+              mode='horizontal'
+              selectedKeys={[getSelectedKey(location.pathname, loggedIn)]}
+              style={{ lineHeight: '64px', width: '100%' }}
             >
-              <Item key='build:1'>
+              <Item key='1'>
+                <Link to='/'>Home</Link>
+              </Item>
+
+              <SubMenu
+                key='2'
+                title={
+                  <span>
+                    Builds
+                    <Icon type='down' style={{ marginLeft: 5, fontSize: 10 }} />
+                  </span>
+                }
+              >
+                <Item key='build:1'>
+                  <Link to='/builds'>Builds</Link>
+                </Item>
+                <Item key='build:2'>
+                  <Link to='/buildEditor/0'>Build Editor</Link>
+                </Item>
+              </SubMenu>
+              <SubMenu
+                key='3'
+                title={
+                  <span>
+                    Raids
+                    <Icon type='down' style={{ marginLeft: 5, fontSize: 10 }} />
+                  </span>
+                }
+              >
+                <Item key='raid:1'>
+                  <Link to='/raids'>Raids</Link>
+                </Item>
+                <Item key='raid:2'>
+                  <Link to='/raidEditor/0'>Raid Editor</Link>
+                </Item>
+              </SubMenu>
+
+              <Item key='4'>
+                <Link to='/overview'>Overview</Link>
+              </Item>
+            </Menu>
+            <Menu
+              theme='light'
+              mode='horizontal'
+              selectedKeys={[
+                location.pathname.includes('/profile') ? 'profile:1' : '',
+              ]}
+              style={{ lineHeight: '64px' }}
+            >
+              <SubMenu
+                key='5'
+                style={{ float: 'right' }}
+                title={
+                  <span>
+                    <Avatar style={{ marginRight: 5 }} />
+                    Hello, {me && me.name}
+                    <Icon type='down' style={{ marginLeft: 5, fontSize: 10 }} />
+                  </span>
+                }
+              >
+                <Item key='profile:1'>
+                  <Link to='/profile'>
+                    <Icon type='user' style={{ marginRight: 8 }} />
+                    Profile
+                  </Link>
+                </Item>
+                <Item onClick={handleLogout}>
+                  <Icon type='logout' />
+                  Log out
+                </Item>
+              </SubMenu>
+            </Menu>
+          </>
+        ) : (
+          <>
+            <Menu
+              theme='light'
+              mode='horizontal'
+              selectedKeys={[getSelectedKey(location.pathname, loggedIn)]}
+              style={{ lineHeight: '64px' }}
+            >
+              <Item key='1'>
+                <Link to='/'>Home</Link>
+              </Item>
+              <Item key='2'>
                 <Link to='/builds'>Builds</Link>
               </Item>
-              <Item key='build:2'>
-                <Link to='/buildEditor/0'>Build Editor</Link>
-              </Item>
-            </SubMenu>
-            <SubMenu
-              key='3'
-              title={
-                <span>
-                  <Icon type='setting' />
-                  Raids
-                  <Icon type='down' style={{ marginLeft: 5, fontSize: 10 }} />
-                </span>
-              }
-            >
-              <Item key='raid:1'>
+              <Item key='3'>
                 <Link to='/raids'>Raids</Link>
               </Item>
-              <Item key='raid:2'>
-                <Link to='/raidEditor/0'>Raid Editor</Link>
+              <Item key='4'>
+                <Link to='/overview'>Overview</Link>
               </Item>
-            </SubMenu>
-
-            <Item key='4'>
-              <Link to='/overview'>Overview</Link>
-            </Item>
-          </Menu>
-        ) : (
-          <Menu
-            theme='light'
-            mode='horizontal'
-            defaultSelectedKeys={[getSelectedKey(location.pathname, loggedIn)]}
-            style={{ lineHeight: '64px' }}
-          >
-            <Item key='1'>
-              <Link to='/'>Home</Link>
-            </Item>
-            <Item key='2'>
-              <Link to='/builds'>Builds</Link>
-            </Item>
-            <Item key='3'>
-              <Link to='/raids'>Raids</Link>
-            </Item>
-            <Item key='4'>
-              <Link to='/overview'>Overview</Link>
-            </Item>
-          </Menu>
+            </Menu>
+            <Popover
+              placement='bottomRight'
+              title='Login'
+              content={<WrappedNormalLoginForm setLoggedIn={setLoggedIn} />}
+              trigger='click'
+            >
+              <Button
+                style={{ float: 'right' }}
+                type='primary'
+                icon='user'
+                size='large'
+              >
+                Login
+              </Button>
+            </Popover>
+          </>
         )}
       </div>
-      {loggedIn ? (
-        <div>
-          <Avatar />
-          <Dropdown overlay={<LogoutMenu handleLogout={handleLogout} />}>
-            <span
-              style={{
-                margin: '0px 10px',
-                cursor: 'pointer',
-              }}
-            >
-              Hello, {me ? me.name : ''}
-              <Icon type='down' style={{ marginLeft: 5 }} />
-            </span>
-          </Dropdown>
-        </div>
-      ) : (
-        <Popover
-          placement='bottomRight'
-          title='Login'
-          content={<WrappedNormalLoginForm setLoggedIn={setLoggedIn} />}
-          trigger='click'
-        >
-          <Button
-            style={{ float: 'right' }}
-            type='primary'
-            icon='user'
-            size='large'
-          >
-            Login
-          </Button>
-        </Popover>
-      )}
     </StyledHeader>
   )
 }
