1c314f381252957b4c363021a83e0e15aa86cd30
Kaisa Huusko <kaisah.huusko@gmail.com>
2019-09-01 13:05:43 +0300

    Prettified

--- src/pages/exploration/raids.tsx
+++ src/pages/exploration/raids.tsx
@@ -1,32 +1,32 @@
-import React from "react";
-import Flex from "../../components/Flex";
-import { MenuCard, ContentCard } from "../overview/Overview";
-import { Input, Divider, Rate, Select, Typography, Slider } from "antd";
-import { applicationAreas, marks } from "../raid/general/RaidGeneral";
+import React from 'react'
+import Flex from '../../components/Flex'
+import { MenuCard, ContentCard } from '../overview/Overview'
+import { Input, Divider, Rate, Select, Typography, Slider } from 'antd'
+import { applicationAreas, marks } from '../raid/general/RaidGeneral'
 
-const { Option } = Select;
-const { Title } = Typography;
+const { Option } = Select
+const { Title } = Typography
 
 export default () => {
   return (
     <Flex
-      direction="row"
-      align="flex-start"
+      direction='row'
+      align='flex-start'
       style={{
-        height: "calc(100vh - 100px)",
-        width: "100%",
+        height: 'calc(100vh - 100px)',
+        width: '100%',
         padding: 20
       }}
     >
-      <MenuCard minWidth="400px">
+      <MenuCard minWidth='400px'>
         <Title level={3}>Browse public raids</Title>
         <Title level={4}>Filters</Title>
-        <Input placeholder="Raid name..."></Input>
+        <Input placeholder='Raid name...'></Input>
         <Divider>Applicaton areas</Divider>
         <Select
-          mode="multiple"
-          style={{ width: "100%" }}
-          placeholder="Select application areas..."
+          mode='multiple'
+          style={{ width: '100%' }}
+          placeholder='Select application areas...'
         >
           {applicationAreas.map((area, index) => (
             <Option key={index}>{area.label}</Option>
@@ -34,9 +34,9 @@ export default () => {
         </Select>
         <Divider>Has roles</Divider>
         <Select
-          mode="multiple"
-          style={{ width: "100%" }}
-          placeholder="Select roles..."
+          mode='multiple'
+          style={{ width: '100%' }}
+          placeholder='Select roles...'
         >
           <Option key={1}>Public role 1</Option>
         </Select>
@@ -55,14 +55,14 @@ export default () => {
       </MenuCard>
       <ContentCard
         bodyStyle={{
-          height: "100%",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center"
+          height: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center'
         }}
       >
         <h1>Raids matching filters</h1>
       </ContentCard>
     </Flex>
-  );
-};
+  )
+}
