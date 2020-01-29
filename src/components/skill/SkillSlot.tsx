344c53a8a228ebaca7ed198afff7b386d4f842e8
Felix Wohnhaas <felix.wohnhaas@outlook.com>
2020-01-29 14:57:52 +0100

    remove unused var

--- src/components/skill/SkillSlot.tsx
+++ src/components/skill/SkillSlot.tsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect } from 'react'
-import styled, { CSSProperties } from 'styled-components'
-import { Popover, Modal } from 'antd'
+import styled from 'styled-components'
+import { Popover } from 'antd'
 import { abilityFrame } from '../../assets/misc'
 import { SkillCardContent } from './SkillCard'
 import { BuildContext } from '../../pages/build/BuildStateContext'
